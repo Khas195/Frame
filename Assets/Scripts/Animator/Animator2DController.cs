e9d4673758fb625d2b1bd25b66bddc70a3629584
Khassian <tungthanhcao1091995@gmail.com>
2021-04-30 14:35:32 +0200

    Rig main character with main character animations

---
+++ Assets/Scripts/Animator/Animator2DController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator2DController : MonoBehaviour
+{
+    [SerializeField]
+    Animator characterAnim = null;
+    [SerializeField]
+    Rigidbody2D characterBody = null;
+    // Update is called once per frame
+    void Update()
+    {
+        characterAnim.SetFloat("Speed", characterBody.velocity.magnitude);
+    }
+}

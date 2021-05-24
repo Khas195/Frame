ed52435d364e78be638cec3af49266b5b8d75b0f
Khassian <tungthanhcao1091995@gmail.com>
2021-05-24 14:25:36 +0200

    add footsteps

--- Assets/Scripts/MusicAndSound/PlayAudioSource.cs
+++ Assets/Scripts/MusicAndSound/PlayAudioSource.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class PlayAudioSource : MonoBehaviour
 {
     [SerializeField]
     AudioSource source;
+    [SerializeField]
+    bool playFromRandomClips = false;
+    [SerializeField]
+    [ShowIf("playFromRandomClips")]
+    List<AudioClip> clips;
+    [SerializeField]
+    [ShowIf("playFromRandomClips")]
+    int lastPlayed = -1;
+
     public void Play()
     {
+        if (playFromRandomClips)
+        {
+            var randIndex = Random.Range(0, clips.Count);
+            do
+            {
+                randIndex = Random.Range(0, clips.Count);
+                source.clip = clips[randIndex];
+
+            } while (randIndex == lastPlayed);
+            lastPlayed = randIndex;
+        }
         source.Play();
     }
     public void Stop()

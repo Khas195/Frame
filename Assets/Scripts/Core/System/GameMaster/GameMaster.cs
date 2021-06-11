ba135739b84b01361d7fbd5292efbd0e37c4b6f3
Khassian <tungthanhcao1091995@gmail.com>
2021-06-11 18:16:45 +0200

    Change transition from Main to Game, Next Day transition and photo loading to  main menu to Photo Transition Change loading taxi for only zone transitions. Hide the photo stats in game

--- Assets/Scripts/Core/System/GameMaster/GameMaster.cs
+++ Assets/Scripts/Core/System/GameMaster/GameMaster.cs
@@ -6,145 +6,145 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameMaster : SingletonMonobehavior<GameMaster>, IObserver
 {
-    [SerializeField]
-    [Required]
-    BuildProfile requiredScenes = null;
-    [SerializeField]
-    [Required]
-    SceneLoadingManager loadingManager = null;
-    [SerializeField]
-    [Required]
-    GameInstance startInstance = null;
-    [SerializeField]
-    [Required]
-    GameInstance mainMenuInstance = null;
-    [SerializeField]
-    [ReadOnly]
-    GameInstance currentInstance = null;
+	[SerializeField]
+	[Required]
+	BuildProfile requiredScenes = null;
+	[SerializeField]
+	[Required]
+	SceneLoadingManager loadingManager = null;
+	[SerializeField]
+	[Required]
+	GameInstance startInstance = null;
+	[SerializeField]
+	[Required]
+	GameInstance mainMenuInstance = null;
+	[SerializeField]
+	[ReadOnly]
+	GameInstance currentInstance = null;
 
 
 
-    [SerializeField]
-    [Required]
-    StateManager gameStates = null;
+	[SerializeField]
+	[Required]
+	StateManager gameStates = null;
 
 
-    public void StartGame()
-    {
-        this.RequestInstance(startInstance);
-    }
+	public void StartGame()
+	{
+		this.RequestInstance(startInstance);
+	}
 
-    /// <summary>
-    /// Awake is called when the script instance is being loaded.
-    /// </summary>
-    protected override void Awake()
-    {
-        base.Awake();
-        PostOffice.Subscribes(this, GameMasterEvent.ON_GAMESTATE_CHANGED);
-    }
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	protected override void Awake()
+	{
+		base.Awake();
+		PostOffice.Subscribes(this, GameMasterEvent.ON_GAMESTATE_CHANGED);
+	}
 
-    public bool RequestGameState(GameState.GameStateEnum requestState)
-    {
-        return this.gameStates.RequestState(requestState);
-    }
+	public bool RequestGameState(GameState.GameStateEnum requestState)
+	{
+		return this.gameStates.RequestState(requestState);
+	}
 
-    void Start()
-    {
-        loadingManager.UnloadAllScenes(exception: requiredScenes.masterScene);
-        loadingManager.LoadSceneAdditively(requiredScenes.logScene);
-        RequestInstance(mainMenuInstance);
-    }
-    void OnDestroy()
-    {
-        PostOffice.Unsubscribes(this, GameMasterEvent.ON_GAMESTATE_CHANGED);
-    }
-
-
-    public void RequestInstance(GameInstance newInstance, bool loadAndWait = true)
-    {
-        if (this.gameStates.RequestState(newInstance.desiredGameState))
-        {
-            loadingManager.InitiateLoadingSequenceFor(newInstance, loadAndWait);
-            currentInstance = newInstance;
-        }
-        else
-        {
-            LogHelper.LogError("Cannot Transition to the desired game state:" + newInstance.desiredGameState + "of this game instance: " + newInstance);
-        }
-    }
+	void Start()
+	{
+		loadingManager.UnloadAllScenes(exception: requiredScenes.masterScene);
+		loadingManager.LoadSceneAdditively(requiredScenes.logScene);
+		RequestInstance(mainMenuInstance);
+	}
+	void OnDestroy()
+	{
+		PostOffice.Unsubscribes(this, GameMasterEvent.ON_GAMESTATE_CHANGED);
+	}
 
 
-    private void NotifyOnGameStateChange(GameState.GameStateEnum newGameState)
-    {
-        var data = DataPool.GetInstance().RequestInstance();
-        data.SetValue(GameMasterEvent.GameStateChangeEvent.New_Game_State, newGameState);
-        PostOffice.SendData(data, GameMasterEvent.ON_GAMESTATE_CHANGED);
-        DataPool.GetInstance().ReturnInstance(data);
-    }
-
-    public GameInstance GetCurrentGameInstance()
-    {
-        return currentInstance;
-    }
-
-    void Update()
-    {
-        var currentState = gameStates.GetCurrentState<GameState>();
-        if (currentState != null)
-        {
-            currentState.UpdateState();
-            if ((GameState.GameStateEnum)currentState.GetEnum() != GameState.GameStateEnum.Console)
-            {
-                ProcessConsoleTrigger();
-            }
-        }
-    }
-    public void ProcessConsoleTrigger()
-    {
-        if (Input.GetKeyDown(KeyCode.F1))
-        {
-            this.gameStates.RequestState(GameState.GameStateEnum.Console);
-        }
-    }
+	public void RequestInstance(GameInstance newInstance, bool loadAndWait = true)
+	{
+		if (this.gameStates.RequestState(newInstance.desiredGameState))
+		{
+			loadingManager.InitiateLoadingSequenceFor(newInstance, loadAndWait);
+			currentInstance = newInstance;
+		}
+		else
+		{
+			LogHelper.LogError("Cannot Transition to the desired game state:" + newInstance.desiredGameState + "of this game instance: " + newInstance);
+		}
+	}
 
 
+	private void NotifyOnGameStateChange(GameState.GameStateEnum newGameState)
+	{
+		var data = DataPool.GetInstance().RequestInstance();
+		data.SetValue(GameMasterEvent.GameStateChangeEvent.New_Game_State, newGameState);
+		PostOffice.SendData(data, GameMasterEvent.ON_GAMESTATE_CHANGED);
+		DataPool.GetInstance().ReturnInstance(data);
+	}
 
-    public void SetMouseVisibility(bool visibility)
-    {
-        Cursor.visible = visibility;
-        Cursor.lockState = visibility ? CursorLockMode.None : CursorLockMode.Confined;
-    }
+	public GameInstance GetCurrentGameInstance()
+	{
+		return currentInstance;
+	}
 
-    public void ExitGame()
-    {
-        // save any game data here
+	void Update()
+	{
+		var currentState = gameStates.GetCurrentState<GameState>();
+		if (currentState != null)
+		{
+			currentState.UpdateState();
+			if ((GameState.GameStateEnum)currentState.GetEnum() != GameState.GameStateEnum.Console)
+			{
+				ProcessConsoleTrigger();
+			}
+		}
+	}
+	public void ProcessConsoleTrigger()
+	{
+		if (Input.GetKeyDown(KeyCode.F1))
+		{
+			this.gameStates.RequestState(GameState.GameStateEnum.Console);
+		}
+	}
+
+
+
+	public void SetMouseVisibility(bool visibility)
+	{
+		Cursor.visible = visibility;
+		Cursor.lockState = visibility ? CursorLockMode.None : CursorLockMode.Confined;
+	}
+
+	public void ExitGame()
+	{
+		// save any game data here
 #if UNITY_EDITOR
-        // Application.Quit() does not work in the editor so
-        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
-        UnityEditor.EditorApplication.isPlaying = false;
+		// Application.Quit() does not work in the editor so
+		// UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+		UnityEditor.EditorApplication.isPlaying = false;
 #else
         Application.Quit();
 #endif
-    }
-    public void SetGameTimeScale(float newTimeScale)
-    {
-        Time.timeScale = newTimeScale;
-    }
+	}
+	public void SetGameTimeScale(float newTimeScale)
+	{
+		Time.timeScale = newTimeScale;
+	}
 
-    public void ReceiveData(DataPack pack, string eventName)
-    {
-    }
-    public void FreezeGame()
-    {
-        Time.timeScale = 0.0f;
-    }
-    public void UnFreezeGame()
-    {
-        Time.timeScale = 1.0f;
-    }
-    public GameState GetCurrentState()
-    {
-        return gameStates.GetCurrentState<GameState>();
-    }
+	public void ReceiveData(DataPack pack, string eventName)
+	{
+	}
+	public void FreezeGame()
+	{
+		Time.timeScale = 0.0f;
+	}
+	public void UnFreezeGame()
+	{
+		Time.timeScale = 1.0f;
+	}
+	public GameState GetCurrentState()
+	{
+		return gameStates.GetCurrentState<GameState>();
+	}
 }
 
--- Assets/Scripts/Core/Ultilities/SwitchGameStats.cs
+++ Assets/Scripts/Core/Ultilities/SwitchGameStats.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ConsoleCommand", menuName = "Console/SwitchGameStats", order = 1)]
 public class SwitchGameStats : ConsoleCommand
 {
-    public static string SWITCH_GAME_STATS_EVENT = "SWITCH_GAME_STATS";
-    public static bool STATS_ON = true;
+	public static string SWITCH_GAME_STATS_EVENT = "SWITCH_GAME_STATS";
+	public static bool STATS_ON = false;
 
-    public override void Execute(InGameLogUI inGameLog = null, string commandLine = "")
-    {
-        STATS_ON = !STATS_ON;
-        PostOffice.SendData(null, SWITCH_GAME_STATS_EVENT);
-    }
+	public override void Execute(InGameLogUI inGameLog = null, string commandLine = "")
+	{
+		STATS_ON = !STATS_ON;
+		PostOffice.SendData(null, SWITCH_GAME_STATS_EVENT);
+	}
 }

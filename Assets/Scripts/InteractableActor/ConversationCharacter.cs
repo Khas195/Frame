80e9fd905aa470c4ded50120026415351801a1de
Khassian <tungthanhcao1091995@gmail.com>
2021-06-14 22:42:00 +0200

    upload 0.7 build

--- Assets/Scripts/InteractableActor/ConversationCharacter.cs
+++ Assets/Scripts/InteractableActor/ConversationCharacter.cs
@@ -8,130 +8,133 @@ using UnityEngine.UI;
 
 public class ConversationCharacter : MonoBehaviour, IParticipant, IObserver
 {
-    [SerializeField]
-    FadeManyTransition textBoxControl = null;
-    [SerializeField]
-    Text textUI = null;
-    [SerializeField]
-    string conversationStitch;
-    [SerializeField]
-    bool isInConvesation = false;
-    bool playerInRange = false;
-    IParticipant playerChar = null;
+	[SerializeField]
+	FadeManyTransition textBoxControl = null;
+	[SerializeField]
+	Text textUI = null;
+	[SerializeField]
+	string conversationStitch;
+	[SerializeField]
+	bool isInConvesation = false;
+	bool playerInRange = false;
+	IParticipant playerChar = null;
 
-    [SerializeField]
-    AudioSource textPrintSound = null;
-    [SerializeField]
-    float letterPause = 0.02f;
-    [SerializeField]
-    [ReadOnly]
-    string textToShow = "";
-    private void Start()
-    {
-        PostOffice.Subscribes(this, GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT);
-    }
-    private void OnDestroy()
-    {
-        PostOffice.Unsubscribes(this, GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT);
-        StopCoroutine("TypeText");
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            playerChar = other.gameObject.GetComponent<IParticipant>();
-            playerInRange = true;
-            ConversationMananger.GetInstance().RequestPlayerConversation(conversationStitch, playerChar, this);
-        }
-    }
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            LogHelper.Log("Conversation Character - player in range");
-            if (InkleManager.GetInstance().GetVariable<bool>("JustPublishedPaper") == true)
-            {
-                LogHelper.Log("Conversation Character - Newspaper just published - reacting");
-                playerChar = other.gameObject.GetComponent<IParticipant>();
-                ConversationMananger.GetInstance().RequestPlayerConversation(conversationStitch, playerChar, this);
-            }
-        }
+	[SerializeField]
+	AudioSource textPrintSound = null;
+	[SerializeField]
+	float letterPause = 0.02f;
+	[SerializeField]
+	[ReadOnly]
+	string textToShow = "";
+	bool reactToPaper = false;
+	private void Start()
+	{
+		PostOffice.Subscribes(this, GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT);
+	}
+	private void OnDestroy()
+	{
+		PostOffice.Unsubscribes(this, GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT);
+		StopCoroutine("TypeText");
+	}
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			playerChar = other.gameObject.GetComponent<IParticipant>();
+			playerInRange = true;
+			ConversationMananger.GetInstance().RequestPlayerConversation(conversationStitch, playerChar, this);
+		}
+	}
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			LogHelper.Log("Conversation Character - player in range");
+			if (InkleManager.GetInstance().GetVariable<bool>("JustPublishedPaper") == true && reactToPaper)
+			{
+				LogHelper.Log("Conversation Character - Newspaper just published - reacting");
+				playerChar = other.gameObject.GetComponent<IParticipant>();
+				ConversationMananger.GetInstance().RequestPlayerConversation(conversationStitch, playerChar, this);
+				reactToPaper = false;
+			}
+		}
 
-    }
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            playerInRange = false;
-            ConversationMananger.GetInstance().TerminateCurrentConversation();
-        }
-    }
-    IEnumerator TypeText()
-    {
-        textUI.text = "";
-        foreach (char letter in textToShow.ToCharArray())
-        {
-            textUI.text += letter;
-            if (textPrintSound != null)
-            {
-                textPrintSound.Play();
-                yield return 0;
-            }
-            yield return new WaitForSeconds(letterPause);
-        }
-    }
+	}
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			playerInRange = false;
+			ConversationMananger.GetInstance().TerminateCurrentConversation();
+		}
+	}
+	IEnumerator TypeText()
+	{
+		textUI.text = "";
+		foreach (char letter in textToShow.ToCharArray())
+		{
+			textUI.text += letter;
+			if (textPrintSound != null)
+			{
+				textPrintSound.Play();
+				yield return 0;
+			}
+			yield return new WaitForSeconds(letterPause);
+		}
+	}
 
-    public void Show(string textToShow)
-    {
-        StopCoroutine("TypeText");
-        textUI.text = "";
-        this.textToShow = textToShow;
-        StartCoroutine("TypeText");
-    }
+	public void Show(string textToShow)
+	{
+		StopCoroutine("TypeText");
+		textUI.text = "";
+		this.textToShow = textToShow;
+		StartCoroutine("TypeText");
+	}
 
-    public void ChooseFromChoices(List<Ink.Runtime.Choice> currentChoices, Action<Choice> choiceCallBack)
-    {
-    }
+	public void ChooseFromChoices(List<Ink.Runtime.Choice> currentChoices, Action<Choice> choiceCallBack)
+	{
+	}
 
-    public void StopConversing()
-    {
-        LogHelper.Log("Conversation-" + this.gameObject.name + "stops conversing");
-        if (textBoxControl.IsFadeOut() == false)
-        {
-            textBoxControl.FadeOut();
-        }
-        isInConvesation = false;
-    }
+	public void StopConversing()
+	{
+		LogHelper.Log("Conversation-" + this.gameObject.name + "stops conversing");
+		if (textBoxControl.IsFadeOut() == false)
+		{
+			textBoxControl.FadeOut();
+		}
+		isInConvesation = false;
+	}
 
-    public void StartConvsering()
-    {
-        LogHelper.Log("Conversation-" + this.gameObject.name + "starts conversing");
-        if (textBoxControl.IsFadeOut())
-        {
-            textBoxControl.FadeIn();
-        }
-        isInConvesation = true;
-    }
+	public void StartConvsering()
+	{
+		LogHelper.Log("Conversation-" + this.gameObject.name + "starts conversing");
+		if (textBoxControl.IsFadeOut())
+		{
+			textBoxControl.FadeIn();
+		}
+		isInConvesation = true;
+	}
 
-    public bool IsInConversation()
-    {
-        return isInConvesation;
-    }
+	public bool IsInConversation()
+	{
+		return isInConvesation;
+	}
 
-    public void RequestStartConversation()
-    {
-        if (playerChar != null && playerInRange)
-        {
-            ConversationMananger.GetInstance().RequestPlayerConversation(conversationStitch, playerChar, this);
-        }
-    }
+	public void RequestStartConversation()
+	{
+		if (playerChar != null && playerInRange)
+		{
+			ConversationMananger.GetInstance().RequestPlayerConversation(conversationStitch, playerChar, this);
+		}
+	}
 
 
-    public void ReceiveData(DataPack pack, string eventName)
-    {
-        if (eventName == GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT)
-        {
-            InkleManager.GetInstance().SetVariable("JustPublishedPaper", true);
-        }
-    }
+	public void ReceiveData(DataPack pack, string eventName)
+	{
+		if (eventName == GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT)
+		{
+			InkleManager.GetInstance().SetVariable("JustPublishedPaper", true);
+			reactToPaper = true;
+		}
+	}
 }
--- Assets/Scripts/Map/TaxiMapControl.cs
+++ Assets/Scripts/Map/TaxiMapControl.cs
@@ -5,110 +5,116 @@ using UnityEngine.UI;
 
 public class TaxiMapControl : MonoBehaviour, IObserver
 {
-    [SerializeField]
-    GameObject mapPanel = null;
-    [SerializeField]
-    AudioSource carDoorOpen = null;
-    [SerializeField]
-    AudioSource carDoorClose = null;
-    [SerializeField]
-    Image playerIcon = null;
-    [SerializeField]
-    Transform picisLocation = null;
-    [SerializeField]
-    Transform parkLocation = null;
-    [SerializeField]
-    GameObject mapNormal = null;
-    [SerializeField]
-    GameObject mapBroken = null;
-    [SerializeField]
-    GameObject parkButtonNormal = null;
-    [SerializeField]
-    GameObject parkBrokenButton = null;
-    [SerializeField]
-    List<ScenarioBranchCondition> conditionsToChangeMap = new List<ScenarioBranchCondition>();
-    [SerializeField]
-    Button picisStreetButton = null;
-    [SerializeField]
-    Button parkButton = null;
-    [SerializeField]
-    Button parkBrokenButtonUI = null;
-    private void Start()
-    {
-        HideMap();
-        PostOffice.Subscribes(this, GameEvent.MapChangedEvent.MAP_CHANGED_EVENT);
-        PostOffice.Subscribes(this, GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT);
-        playerIcon.transform.position = picisLocation.position;
-        mapNormal.gameObject.SetActive(true);
-        mapBroken.gameObject.SetActive(false);
-        parkButtonNormal.gameObject.SetActive(true);
-        parkBrokenButton.gameObject.SetActive(false);
-    }
-    public void ShowMap()
-    {
-        mapPanel.SetActive(true);
-        ConversationMananger.GetInstance().TerminateCurrentConversation();
-    }
-    public void HideMap()
-    {
-        mapPanel.SetActive(false);
-    }
-    public void ChangeMapToBroken()
-    {
-        LogHelper.Log("Map Control - Map broken");
-        this.mapNormal.gameObject.SetActive(false);
-        this.mapBroken.gameObject.SetActive(true);
-        parkButtonNormal.gameObject.SetActive(false);
-        parkBrokenButton.gameObject.SetActive(true);
-    }
-    public void LoadInstance(GameInstance instance)
-    {
-        var gameMaster = GameMaster.GetInstance();
-        if (gameMaster.GetCurrentGameInstance() == instance) return;
+	[SerializeField]
+	GameObject mapPanel = null;
+	[SerializeField]
+	AudioSource carDoorOpen = null;
+	[SerializeField]
+	AudioSource carDoorClose = null;
+	[SerializeField]
+	Image playerIcon = null;
+	[SerializeField]
+	Transform picisLocation = null;
+	[SerializeField]
+	Transform parkLocation = null;
+	[SerializeField]
+	GameObject mapNormal = null;
+	[SerializeField]
+	GameObject mapBroken = null;
+	[SerializeField]
+	GameObject parkButtonNormal = null;
+	[SerializeField]
+	GameObject parkBrokenButton = null;
+	[SerializeField]
+	List<ScenarioBranchCondition> conditionsToChangeMap = new List<ScenarioBranchCondition>();
+	[SerializeField]
+	Button picisStreetButton = null;
+	[SerializeField]
+	Button parkButton = null;
+	[SerializeField]
+	Button parkBrokenButtonUI = null;
+	private void Start()
+	{
+		HideMap();
+		PostOffice.Subscribes(this, GameEvent.MapChangedEvent.MAP_CHANGED_EVENT);
+		PostOffice.Subscribes(this, GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT);
+		playerIcon.transform.position = picisLocation.position;
+		mapNormal.gameObject.SetActive(true);
+		mapBroken.gameObject.SetActive(false);
+		parkButtonNormal.gameObject.SetActive(true);
+		parkBrokenButton.gameObject.SetActive(false);
+	}
+	private void OnDestroy()
+	{
+		PostOffice.Unsubscribes(this, GameEvent.MapChangedEvent.MAP_CHANGED_EVENT);
+		PostOffice.Unsubscribes(this, GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT);
 
-        gameMaster.RequestInstance(instance);
-        InGameUIControl.GetInstance().RequestState(InGameUIState.InGameUIStateEnum.NormalState);
-        carDoorOpen.Play();
-        carDoorClose.PlayDelayed(carDoorOpen.clip.length);
-    }
+	}
+	public void ShowMap()
+	{
+		mapPanel.SetActive(true);
+		ConversationMananger.GetInstance().TerminateCurrentConversation();
+	}
+	public void HideMap()
+	{
+		mapPanel.SetActive(false);
+	}
+	public void ChangeMapToBroken()
+	{
+		LogHelper.Log("Map Control - Map broken");
+		this.mapNormal.gameObject.SetActive(false);
+		this.mapBroken.gameObject.SetActive(true);
+		parkButtonNormal.gameObject.SetActive(false);
+		parkBrokenButton.gameObject.SetActive(true);
+	}
+	public void LoadInstance(GameInstance instance)
+	{
+		var gameMaster = GameMaster.GetInstance();
+		if (gameMaster.GetCurrentGameInstance() == instance) return;
 
-    public void ReceiveData(DataPack pack, string eventName)
-    {
-        if (eventName.Equals(GameEvent.MapChangedEvent.MAP_CHANGED_EVENT))
-        {
-            var currentLocaton = pack.GetValue<GameEvent.MapChangedEvent.MapLocation>(GameEvent.MapChangedEvent.MAP_LOCATION_DATA);
-            if (currentLocaton == GameEvent.MapChangedEvent.MapLocation.PicisStreet)
-            {
-                picisStreetButton.interactable = false;
-                parkBrokenButtonUI.interactable = true;
-                parkButton.interactable = true;
-                playerIcon.transform.position = picisLocation.position;
-            }
-            else
-            {
-                picisStreetButton.interactable = true;
-                parkBrokenButtonUI.interactable = false;
-                parkButton.interactable = false;
-                playerIcon.transform.position = parkLocation.position;
-            }
-        }
-        if (eventName.Equals(GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT))
-        {
-            bool condition = true;
+		gameMaster.RequestInstance(instance);
+		InGameUIControl.GetInstance().RequestState(InGameUIState.InGameUIStateEnum.NormalState);
+		carDoorOpen.Play();
+		carDoorClose.PlayDelayed(carDoorOpen.clip.length);
+	}
 
-            conditionsToChangeMap.ForLoop<ScenarioBranchCondition>((ScenarioBranchCondition curCondition) =>
-            {
-                LogHelper.Log("Map Control - Checking branching conditions:" + curCondition.name + "- " + curCondition.IsSatisfied());
-                if (curCondition.IsSatisfied() == false)
-                {
-                    condition = false;
-                }
-            });
+	public void ReceiveData(DataPack pack, string eventName)
+	{
+		if (eventName.Equals(GameEvent.MapChangedEvent.MAP_CHANGED_EVENT))
+		{
+			var currentLocaton = pack.GetValue<GameEvent.MapChangedEvent.MapLocation>(GameEvent.MapChangedEvent.MAP_LOCATION_DATA);
+			if (currentLocaton == GameEvent.MapChangedEvent.MapLocation.PicisStreet)
+			{
+				picisStreetButton.interactable = false;
+				parkBrokenButtonUI.interactable = true;
+				parkButton.interactable = true;
+				playerIcon.transform.position = picisLocation.position;
+			}
+			else
+			{
+				picisStreetButton.interactable = true;
+				parkBrokenButtonUI.interactable = false;
+				parkButton.interactable = false;
+				playerIcon.transform.position = parkLocation.position;
+			}
+		}
+		if (eventName.Equals(GameEvent.NewspaperEvent.NEWSPAPER_PUBLISHED_EVENT))
+		{
+			bool condition = true;
 
-            if (condition == true)
-            {
-                ChangeMapToBroken();
-            }
-        }
-    }
+			conditionsToChangeMap.ForLoop<ScenarioBranchCondition>((ScenarioBranchCondition curCondition) =>
+			{
+				LogHelper.Log("Map Control - Checking branching conditions:" + curCondition.name + "- " + curCondition.IsSatisfied());
+				if (curCondition.IsSatisfied() == false)
+				{
+					condition = false;
+				}
+			});
+
+			if (condition == true)
+			{
+				ChangeMapToBroken();
+			}
+		}
+	}
 }
